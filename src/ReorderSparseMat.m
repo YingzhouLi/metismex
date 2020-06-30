48b06ca9b53325acfec3da4bcbc5bf50f336d59d
Yingzhou Li <yingzhouli0417@gmail.com>
2020-06-30 17:03:03 -0400

    Updated date range of license

--- src/ReorderSparseMat.m
+++ src/ReorderSparseMat.m
@@ -10,7 +10,7 @@ function [perm,iperm] = ReorderSparseMat(A,options)
 %
 %   See also METIS_PARAMETERS, METIS_NODEND.
 
-%   Copyright (c) 2015-2016 Yingzhou Li, Stanford University
+%   Copyright (c) 2015-2020 Yingzhou Li
 
 if nargin < 2
     options = [];
